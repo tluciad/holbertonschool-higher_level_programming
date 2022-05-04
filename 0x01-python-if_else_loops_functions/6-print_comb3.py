bf2edae9623cb0dbc939e4d3aaca9b5d63c6d02d
tluciad <tluciad@gmail.com>
2022-05-03 20:20:31 -0500

    print combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for numbers in range(0, 10):
+    for numbers2 in range(0, 10):
+        if numbers < numbers2 and numbers != numbers2:
+            print(f"{numbers}{numbers2}", end=", ")
+print(89)
