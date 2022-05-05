7f77d9b1dfad8120e8fac6f041f73be7033e7750
tluciad <tluciad@gmail.com>
2022-05-05 17:22:10 -0500

    list argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+if __name__ == "__main__":
+    from sys import argv
+    print("{}".format(len(argv) - 1), end=" ")
+    if len(argv) - 1 == 0:
+        print("arguments.")
+    elif len(argv) - 1 == 1:
+        print("argument:")
+    else:
+        print("arguments:")
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
