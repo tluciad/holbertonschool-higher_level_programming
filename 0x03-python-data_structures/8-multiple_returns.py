03073366574dbbbffb8dfa129c446dffec594fd8
tluciad <tluciad@gmail.com>
2022-05-09 23:05:23 -0500

    len+first

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     tuple1 = (length, first)
-    if sentence == "":
-        first is None
+    if not sentence:
+        sentence = None
     else:
         return tuple1
