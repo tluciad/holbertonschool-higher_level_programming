ec474ca15dc652f56647fdba0a4f0c002f0ca29f
tluciad <tluciad@gmail.com>
2022-05-23 15:50:56 -0500

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    j = 0
+    for j in range(0, x):
+        try:
+            print("{}".format(my_list[j]), end="")
+            i += 1
+        except:
+            pass
+    print("")
+    return i
