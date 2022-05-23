386e857e6c646542fb617326999124e4f538b5e1
tluciad <tluciad@gmail.com>
2022-05-23 17:03:39 -0500

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,6 +4,7 @@ from unittest import result
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
+    i = 0
     for i in range(list_length):
         try:
             result = my_list_1[i]/my_list_2[i]
@@ -17,5 +18,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result = 0
         finally:
+            i += 0
             new_list.append(result)
     return(new_list)
