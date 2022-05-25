ecc4c9b5f89fd53ad9987df434609467b52377b1
tluciad <tluciad@gmail.com>
2022-05-24 19:33:48 -0500

    a private instance atribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Creating the class square"""
+
+    def __init__(self, size):
+        """defining a private instance atrubute: size"""
+        self.__size = size
