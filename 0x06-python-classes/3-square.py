72f12610a35ddace20f4cc421060d6b827775312
tluciad <tluciad@gmail.com>
2022-05-24 20:35:02 -0500

    public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a class that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """Creating the class square"""
+
+    def __init__(self, size=0):
+        """defining a private instance atribute: size"""
+        if type(size) != int:
+            """size must be an integer"""
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            """size must be most than 0"""
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Public instance method"""
+        a = self.__size*self.__size
+        """to return the area"""
+        return a
