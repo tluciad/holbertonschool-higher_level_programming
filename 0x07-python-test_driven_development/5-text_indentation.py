e0b0de6fdc9b30e2519340986fb9b8fe4ce7310a
tluciad <tluciad@gmail.com>
2022-05-29 20:48:45 -0500

    edit a string paragraph

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""function that prints a text with 2 new lines after 
+each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    if type(text) != str:
+        """text must be a string"""
+        raise TypeError("text must be a string")
+    else:
+        newtxt = text
+        newtxt = newtxt.replace(".", ".\n\n")
+        newtxt = newtxt.replace(",", ",\n\n")
+        newtxt = newtxt.replace(":", ":\n\n")
+        newtxt = newtxt.split("\n")
+    for line in newtxt:
+        line = line.strip()
+        print(line + "\n", end="")
