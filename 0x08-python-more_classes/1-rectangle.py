388c2768ede2ff0624b33a56a2d2e1e96232dec0
tluciad <tluciad@gmail.com>
2022-05-31 15:01:19 -0500

    rectangle width - height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle by: (based on 1-rectangle.py)"""
+
+
+class Rectangle:
+    """empty class that defines a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """property def width(self): to retrieve it"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """property def height(self): to retrieve it"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
