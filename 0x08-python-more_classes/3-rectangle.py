a3cc362bf79d5a3da7fec3847d78f2468586ea33
tluciad <tluciad@gmail.com>
2022-05-31 18:57:02 -0500

    pycodestyle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class Rectangle that defines a rectangle by: (based on 1-rectangle.py)"""
+"""class Rectangle that defines a rectangle by: (based on 3-rectangle.py)"""
 
 
 class Rectangle:
