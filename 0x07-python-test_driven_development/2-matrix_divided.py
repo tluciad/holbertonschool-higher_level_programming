b5fd651d478bd3b1674f79172f1016e73de1905b
tluciad <tluciad@gmail.com>
2022-05-31 10:22:28 -0500

    matrix divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,3 +22,11 @@ def matrix_divided(matrix, div):
     if type(matrix[0][0]) != int and type(matrix[0][0]) != float:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for j in range(len(matrix[i])):
+            new_matrix[i].append(round(matrix[i][j] / div, 2))
+    return new_matrix
