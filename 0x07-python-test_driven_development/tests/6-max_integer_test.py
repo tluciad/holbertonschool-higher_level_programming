b9a96ecb12b90353941de8b93c668f9b57bc73b5
tluciad <tluciad@gmail.com>
2022-05-31 09:53:05 -0500

    missing test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -26,6 +26,21 @@ class TestMaxInteger(unittest.TestCase):
         """to check with floats in the list"""
         self.assertEqual(max_integer([-50.5, -7.2, 8, 3.5, 9]), 9)
         self.assertEqual(max_integer([-50, -7.3, -8.2, -3.1]), -3.1)
+    
+    def text_max_integer5(self):
+        """to check max at the beggining"""
+        self.assertEqual(max_integer([12, 3, 1, 4]), 12)
+        self.assertEqual(max_integer([-1, -7.3, -8.2, -3.1]), -1)
+
+    def text_max_integer6(self):
+        """to check max list with one element"""
+        self.assertEqual(max_integer([8]), 8)
+    
+    def text_max_integer5(self):
+        """to check max just negatives integers"""
+        self.assertEqual(max_integer([-12, -3, -1, -4]), -1)
+        self.assertEqual(max_integer([-7, -2, -5]), -2)
+      
 
 
 if __name__ == '__main__':
