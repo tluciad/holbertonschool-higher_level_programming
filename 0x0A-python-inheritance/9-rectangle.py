b95308b6e1793734f4f2ee3659154904d3700b35
tluciad <tluciad@gmail.com>
2022-06-07 22:30:13 -0500

    implementing area

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -20,4 +20,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """return, the following rectangle
         description: [Rectangle] <width>/<height>"""
-        return("[Rectangle] {}/{}".format(self.__height, self.__width))
+        return("[Rectangle] {}/{}".format(self.__width, self.__height))
