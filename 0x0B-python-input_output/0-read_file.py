1585ae682a2105c3e5dea27023a77b5a22e9361e
tluciad <tluciad@gmail.com>
2022-06-08 16:56:45 -0500

    reading and printing a txt file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     with open(filename, 'r', encoding="utf-8") as f:
         """must use the with statement"""
         read_data = f.read()
-        print(read_data, end=" ")
+        print(read_data)
