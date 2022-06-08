f2c3b948e8635e8cfb5c98da2e05eb0fe0fa2a89
tluciad <tluciad@gmail.com>
2022-06-08 12:31:35 -0500

    reading and printing a txt file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8)
+and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Prototype of function"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        """must use the with statement"""
+        read_data = f.read()
+        print(read_data, end=" ")
