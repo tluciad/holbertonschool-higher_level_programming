28eddb35d20bbd732f2bd4975ce59532fec89bfb
tluciad <tluciad@gmail.com>
2022-06-08 19:41:10 -0500

    save to json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that writes an Object to a text file
+using a JSON representation:"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Prototype of function"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return (json.dump(my_obj, f))
