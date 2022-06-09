f6b39e315476d3239d6eb298cba3e7c69b488006
tluciad <tluciad@gmail.com>
2022-06-08 20:29:54 -0500

    creates an object from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that creates an Object from a “JSON file”"""
+import json
+
+
+def load_from_json_file(filename):
+    """Prototype of function"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.load(file)
