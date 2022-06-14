5638e1a45c276322174925dad7c87651305075de
tluciad <tluciad@gmail.com>
2022-06-14 17:42:56 -0500

    almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,24 +4,32 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """a class Square that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        """super class with id, x, y, width and height"""
 
     @property
     def size(self):
+        """getter size square"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """setter size square"""
         self.width = value
         self.height = value
 
     def __str__(self):
+        """method should return [Square] (<id>) <x>/<y>
+        <size> - in our case, width or height"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y,
                                                  self.width)
 
     def update(self, *args, **kwargs):
+        """adding the public method
+        that assigns attributes"""
         if args is not None and len(args) is not 0:
             list = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
@@ -31,4 +39,6 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """Square by adding the public method
+        that returns the dictionary"""
         return {'id': self.id, 'x': self.x, 'y': self.y, 'size': self.size}
