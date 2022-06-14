1538ab565264e36d65c38ded3250bd8c612bfd4b
tluciad <tluciad@gmail.com>
2022-06-14 17:23:22 -0500

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """class Rectangle that inherits from Base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Private instance attributes"""
         self.width = width
         self.height = height
         self.x = x
@@ -13,10 +16,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter of width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter of width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -25,10 +30,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """getter of height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter of height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -37,10 +44,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """getter of x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter of x"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -49,10 +58,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """getter of y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter of y"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -62,8 +73,12 @@ class Rectangle(Base):
     def area(self):
         """ adding the public method"""
         return(self.__height*self.__width)
+        """adding the public method returns the
+        area value of the Rectangle"""
 
     def display(self):
+        """adding the public method that prints
+        in stdout the Rectangle"""
         if self.__y > 0:
             for i in range(self.__y):
                 print()
@@ -80,6 +95,8 @@ class Rectangle(Base):
             self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
+        """public method that assigns an argument
+        to each attribute"""
         if len(args) == 0:
             for key in kwargs:
                 setattr(self, key, kwargs[key])
@@ -95,5 +112,6 @@ class Rectangle(Base):
                 setattr(self, tupla[i], args[i])
 
     def to_dictionary(self):
+        """ that returns the JSON string representation of
+        list_dictionaries"""
         return vars(self)
-        
