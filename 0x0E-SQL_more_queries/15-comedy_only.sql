664c7ce23add346d16fa7de15344ce580fe76bbf
tluciad <tluciad@gmail.com>
2022-07-15 23:46:00 -0500

    15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Import the database dump from hbtn_0d_tvshows 
+-- to your MySQL server: download script that uses the hbtn_0d_tvshows 
+-- database to lists all genres of the show Dexter.
+SELECT tv_shows.title AS title FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
