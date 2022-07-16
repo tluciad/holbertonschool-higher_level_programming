e14fc6502daa3ee4c175a9e69cecfb2b5a4b68ae
tluciad <tluciad@gmail.com>
2022-07-15 23:07:17 -0500

    14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- mport the database dump from hbtn_0d_tvshows 
+-- to your MySQL server: download
+SELECT tv_genres.name AS name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id 
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter";
