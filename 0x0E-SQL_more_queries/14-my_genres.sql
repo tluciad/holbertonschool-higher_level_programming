93c236c94ffa26a2919d84a59158469ab33cb9bc
tluciad <tluciad@gmail.com>
2022-07-15 23:47:56 -0500

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,6 @@
 -- to your MySQL server: download
 SELECT tv_genres.name AS name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id 
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter";
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_shows.title = "Dexter";
+ORDER BY tv_genres.name;
