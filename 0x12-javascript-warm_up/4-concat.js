1a291d3ba9708a04f14a4e301dd9c74efba6592e
tluciad <tluciad@gmail.com>
2022-08-15 15:38:51 -0500

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// a script that prints two arguments passed to it,
+// in the following format: “ is ”
+const myVar0 = ' is ';
+console.log(process.argv[2] + myVar0 + process.argv[3]);
