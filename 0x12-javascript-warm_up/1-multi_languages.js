d223025fd0ca7805da39e347f3338319144afce0
tluciad <tluciad@gmail.com>
2022-08-15 13:41:25 -0500

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines:
+const myVar1 = 'C is fun\n';
+const myVar2 = 'Python is cool\n';
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar1 + myVar2 + myVar3);
