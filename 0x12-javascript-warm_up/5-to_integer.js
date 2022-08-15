4843e1ddefd27a8e7ef1b2eef518f8f1f620bb63
tluciad <tluciad@gmail.com>
2022-08-15 16:47:11 -0500

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-//
+/* a script that prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer: */
 const myVar0 = process.argv[2];
 const myVar1 = 'My number: ';
 if (isNaN(myVar0)) {
