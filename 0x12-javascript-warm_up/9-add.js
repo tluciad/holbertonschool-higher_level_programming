38a49ea06ca0406b326cba2ded41fe3d1fce406b
tluciad <tluciad@gmail.com>
2022-08-15 22:09:16 -0500

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a script that prints the addition of 2 integers
+const a = process.argv[2];
+const b = process.argv[3];
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return (NaN);
+  }
+  return (parseInt(a) + parseInt(b));
+}
+console.log(add(a, b));
