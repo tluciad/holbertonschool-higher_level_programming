75ca8fe7aef03a9eb26e38c08432a26a77f29b71
tluciad <tluciad@gmail.com>
2022-08-15 19:47:28 -0500

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@
 const x = process.argv[2];
 let i;
 if (isNaN(x)) {
-    console.log('Missing size');
+  console.log('Missing size');
 }
 for (i = 0; i < x; i++) {
-    console.log('X'.repeat(x));
+  console.log('X'.repeat(x));
 }
