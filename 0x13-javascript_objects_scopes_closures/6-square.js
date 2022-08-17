7621ee3655788a956aa9eae7e5300c8ff4eb8b20
tluciad <tluciad@gmail.com>
2022-08-17 16:57:55 -0500

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ module.exports = class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-        this.print();
+      this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
