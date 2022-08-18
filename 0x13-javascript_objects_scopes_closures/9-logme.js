523422be95341acd448745c63e2a5a31ffb12422
tluciad <tluciad@gmail.com>
2022-08-18 09:47:14 -0500

    9-logme.js

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* function that prints the number of arguments
+already printed and the new argument value */
+exports.logMe = function (item) {
+  let l = 0;
+  console.log(l + ': ' + item);
+  l++;
+};
