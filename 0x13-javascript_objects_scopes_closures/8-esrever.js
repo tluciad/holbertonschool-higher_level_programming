304b1a420ab15140774b24e308e63f5d6e71c062
tluciad <tluciad@gmail.com>
2022-08-18 09:28:38 -0500

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // function that returns the reversed version of a list:
 exports.esrever = function (list) {
-    let revArray = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-      revArray.push(list[i]);
-    }
-    return revArray;
-  };
+  const revArray = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    revArray.push(list[i]);
+  }
+  return revArray;
+};
