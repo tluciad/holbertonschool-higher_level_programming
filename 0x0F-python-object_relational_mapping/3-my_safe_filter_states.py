fe33d75349b1f2112987595d2e410a4328a7b659
tluciad <tluciad@gmail.com>
2022-08-30 18:05:52 -0500

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa:"""
+
+if __name__ == '__main__':
+    """module to order the states by id ASC"""
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    mycursor = db.cursor()
+    st = sys.argv[4].split('\'')
+
+    sql = "SELECT * FROM states WHERE name = '{}' ORDER BY \
+states.id ASC".format(st[0])
+
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+
+    for x in myresult:
+        print(x)
