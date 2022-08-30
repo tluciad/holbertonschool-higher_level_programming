f96a5c7dfe69bfcc24b57e6de8fe40684bc0b678
tluciad <tluciad@gmail.com>
2022-08-30 15:56:12 -0500

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,22 +4,22 @@
 if __name__ == '__main__':
     """module to order the states by id ASC"""
 
-import MySQLdb
-import sys
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(
-    host='localhost',
-    user=sys.argv[1],
-    password=sys.argv[2],
-    database=sys.argv[3]
-)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
 
-mycursor = db.cursor()
+    mycursor = db.cursor()
 
-sql = "SELECT * FROM states ORDER BY states.id ASC"
+    sql = "SELECT * FROM states ORDER BY states.id ASC"
 
-mycursor.execute(sql)
-myresult = mycursor.fetchall()
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
 
-for x in myresult:
-    print(x)
+    for x in myresult:
+        print(x)
