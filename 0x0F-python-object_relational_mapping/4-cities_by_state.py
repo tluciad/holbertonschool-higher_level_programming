c0de4cb0220c8e12784051b5979f0a8ab6dbdcfa
tluciad <tluciad@gmail.com>
2022-08-30 19:15:28 -0500

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     )
 
     mycursor = db.cursor()
-      
     mycursor.execute("SELECT cities.id, cities.name, states.name \
 FROM cities JOIN states ON cities.state_id = states.id \
 ORDER BY cities.id ASC")
