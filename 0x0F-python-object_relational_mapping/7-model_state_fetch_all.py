f3125f732ef2873d3ff2031f3b759acaab83274c
tluciad <tluciad@gmail.com>
2022-08-31 17:18:05 -0500

    7-model_state_fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,6 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     """module to order sorted in ascending order by states.id"""
 
-
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
                                                     sys.argv[2],
