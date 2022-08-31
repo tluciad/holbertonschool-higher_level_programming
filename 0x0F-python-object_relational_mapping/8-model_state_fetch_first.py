4738abcd183d715efd4e97fb3b372169f57bda0f
tluciad <tluciad@gmail.com>
2022-08-31 18:00:02 -0500

    8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""""lists all State objects from the database hbtn_0e_6_usa"""
+
+if __name__ == '__main__':
+    """module to order display 1rst state"""
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3])
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+
+    print("{}: {}".format(state.id, state.name))
+
+    session.close()
