7bec05cbaefef780cf592cf725765be15371ae9f
tluciad <tluciad@gmail.com>
2022-08-31 17:19:48 -0500

    7-model_state_fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """"lists all State objects from the database hbtn_0e_6_usa"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     """module to order sorted in ascending order by states.id"""
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
@@ -14,11 +15,11 @@ if __name__ == '__main__':
                                                     sys.argv[3])
     )
 
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for state in session.query(State).order_by(State.id):
-    print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
 
-session.close()
+    session.close()
