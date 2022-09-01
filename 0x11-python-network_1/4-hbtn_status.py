e708845aa74bc6b079c060c8a26f868f48e77180
tluciad <tluciad@gmail.com>
2022-09-01 18:08:04 -0500

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status"""
+import requests
+if __name__ == '__main__':
+    """module to display like the example"""
+    """Body response:$
+    - type: <class 'str'>$
+    - content: OK$"""
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(r.text.__class__))
+    print("\t- content: {}".format(r.text))
