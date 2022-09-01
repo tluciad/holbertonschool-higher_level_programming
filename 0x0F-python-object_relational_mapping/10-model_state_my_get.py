a601949e772dac3e6ddcea7d52159466bf77059d
tluciad <tluciad@gmail.com>
2022-08-31 19:32:17 -0500

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""""lists all State objects from the database hbtn_0e_6_usa"""
+
+if __name__ == '__main__':
+    """module to order display 1rst state"""
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3])
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(name=sys.argv[4]).first()
+    if state is None:
+        print("Not found")
+
+    print(state.id)
+
+    session.close()
